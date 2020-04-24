c16c6c91ca7c2587160a1ad431307f3481272ad9
Jesper Petersen <info@ordklar.dk>
2020-04-24 14:11:22 +0200

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text("Hello world"),
-      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('First app'),
+          centerTitle: true,
+          backgroundColor: Colors.blueAccent[800],//Hvis den er 200 så er den meget lys men hvis den er 900 så er den meget mørk.
+        ),
+        body: Center(
+          child: Image(
+            image: NetworkImage("https://www.w3schools.com/w3css/img_lights.jpg"),
+          ),
+        ),
+      )
     )
   );
 }
